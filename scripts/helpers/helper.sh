76d6d5640a18172b8af010ca9795f9ab267863fb
vagrant <vagrant@localhost.localdomain>
2015-09-12 14:13:30 +0000

    rbenv

---
+++ scripts/helpers/helper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function git_clone_or_update() {
+  local repo=$1
+  local home=$2
+
+  if [ -d "$home" ]; then
+    ( cd $home && git pull origin `git name-rev --name-only HEAD` )
+  else
+    git clone $repo $home
+  fi
+}
